2847b78484ad07f8c970c4343b9c6bb75a5b302a
Christoph Kiank <vchoid@googlemail.com>
2018-03-08 15:12:05 +0100

    Port-Klasse aufgräumt

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -34,17 +34,18 @@ public class SampleController implements Initializable {
 	private TextField ipAddr4TField;
 	@FXML
 	private ChoiceBox<String> serverChoiceBox;
-	
-	private Server s;
+
+	private Server sOld;
+	private Server sNew;
 	private String serverName;
 	private String ipTFieldConcat;
 	private String[] ipPartsAdd;
 	private String[] ipPartsEdit;
 	private String ipEditConcat;
-	
+
 	private String ip;
 	private String[] ipArr;
-	
+
 	private boolean isServerEditActiv;
 	private boolean isServerChoiceBoxActiv;
 
@@ -56,95 +57,23 @@ public class SampleController implements Initializable {
 	@FXML
 	private ChoiceBox<String> portChoiceBox;
 	private boolean isPortEditActiv;
-	private boolean isPortChoiceBoxActiv;
 
 	private Port pOld;
 	private Port pNew;
-	private String portName;
-	private String portAddr;
-	// #########################################################################
-	// ## Server ###############################################################
-	// #########################################################################
 
-	/**
-	 * Überprüft ob gültige Werte in den Feldern geschrieben wurden.
-	 * 
-	 * @return true wenn Feld nicht leer und gültige IP-Adresse
-	 */
-	private boolean isSAddrFieldValid() {
-		if (!serverNameTField.getText().isEmpty() || !ipAddr1TField.getText().isEmpty()
-				|| !ipAddr2TField.getText().isEmpty() || !ipAddr3TField.getText().isEmpty()
-				|| !ipAddr4TField.getText().isEmpty()) {
-			ipTFieldConcat = ipAddr1TField.getText() + "." + ipAddr2TField.getText() + "." + ipAddr3TField.getText() + "."
-					+ ipAddr4TField.getText();
-			if (rv.validateIP(ipTFieldConcat)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	public void clearServerField() {
-		serverNameTField.clear();
-		ipAddr1TField.clear();
-		ipAddr2TField.clear();
-		ipAddr3TField.clear();
-		ipAddr4TField.clear();
-	}
-	public void updateServerList() {
-		jfList.saveServerValuesInAList();
-		serverChoiceBox.setItems(jfList.getServerNameList());
-	}
-	// << Delete >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	public void delServerEntry() {
-		jfList.deleteServer(serverChoiceBox.getValue());
+	// #########################################################################
+	// ## Init #################################################################
+	// #########################################################################
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		jfList.init();
+		updatePortList();
 		updateServerList();
 	}
-	// << Edit >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	public void getServerChoiceBoxValues() {
-		serverName = serverChoiceBox.getValue();
-		serverNameTField.setText(serverName);
-		ipArr = jfList.makeArrayFromIp(jfList.searchValueByName(jfList.getServerArray(), serverName, "ip"));
-		ipAddr1TField.setText(ipArr[0]);
-		ipAddr2TField.setText(ipArr[1]);
-		ipAddr3TField.setText(ipArr[2]);
-		ipAddr4TField.setText(ipArr[3]);
-		setServerEditActiv(true);
-		System.out.println(isServerEditActiv);
-	}
-	public void activateServerEdit() {
-		setServerChoiceBoxActiv(true);
-		System.out.println(isServerChoiceBoxActiv);
-	}
-	public void editServerEntry() {
-//		jfList.editServerName(serverName, serverNameTField.getText());
-		System.out.println(ipArr[0]);
-		String[] t = jfList.makeArrayFromIp(serverNameTField.getText());
-		System.out.println(t[0]);
-//		jfList.editServerIP(ipArr, jfList.makeArrayFromIp(serverNameTField.getText()));
-	}
-	// << Add >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	public void saveServerEntry() {
-		if (isSAddrFieldValid()) {
-			s = new Server(serverNameTField.getText());
-			s.setIPcreateHost(ipTFieldConcat);
-			jfList.init();
-			System.out.println(isServerChoiceBoxActiv() && isServerEditActiv());
-			if(isServerChoiceBoxActiv() && isServerEditActiv()) {
-				editServerEntry();
-				setServerChoiceBoxActiv(false);
-				setServerEditActiv(false);
-			}
-			jfList.addServer(s);
-			updateServerList();
-			System.out.println(jfList.getServerArray());
-			messageLabel.setText(jfList.getExcMessage());
-		}
-		clearServerField();
-	}
+
 	// #########################################################################
 	// ## Port #################################################################
 	// #########################################################################
-
 	/**
 	 * Überprüft ob gültige Werte in den Feldern geschrieben wurden.
 	 * 
@@ -159,17 +88,27 @@ public class SampleController implements Initializable {
 		return false;
 	}
 
+	/**
+	 * Leert die Eingabefelder
+	 */
 	public void clearPortField() {
 		portNameTField.clear();
 		portAddrTField.clear();
+		setPortEditActiv(false);
 	}
 
+	/**
+	 * Aktualisiert den Inhalt der ChoiceBox für die Ports.
+	 */
 	public void updatePortList() {
 		jfList.savePortValuesInAList();
 		portChoiceBox.setItems(jfList.getPortNameList());
 	}
 
 	// << Delete >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	/**
+	 * Löscht einen Port aus der JSON-Datei
+	 */
 	public void delportEntry() {
 		jfList.deletePort(portChoiceBox.getValue());
 		setPortEditActiv(false);
@@ -178,51 +117,116 @@ public class SampleController implements Initializable {
 	}
 
 	// << Edit >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-	public void getPortChoiceBoxValues() {
-		portName = portChoiceBox.getValue();
-		portNameTField.setText(portName);
-		portAddr = jfList.searchValueByName(jfList.getPortsArray(), portName, "port");
-		portAddrTField.setText(portAddr);
+	/**
+	 * Holt den zu bearbeitenden Port und fügt ihn ihn die Eingabemaske ein.
+	 * Anschließend wird der Status Bearbeiten auf true gesetzt,
+	 */
+	public void editPortEntry() {
+		pOld = new Port(portChoiceBox.getValue());
+		pOld.createPort(jfList.searchValueByName(jfList.getPortsArray(), pOld.getName(), "port"));
+		portNameTField.setText(pOld.getName());
+		portAddrTField.setText(pOld.getPort());
 		setPortEditActiv(true);
 	}
 
-	public void editPortEntry() {
-		pOld = new Port(portName);
-		pOld.createPort(portAddr);
-		pNew = new Port(portNameTField.getText());
-		pNew.createPort(portAddrTField.getText());
-		jfList.editPort(pOld, pNew);
-	}
-
-	public void activatePortEdit() {
-		setPortChoiceBoxActiv(true);
-	}
-
 	// << Add >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	/**
+	 * Überprüft vor dem Einfügen eines neuen Ports ob die Felder gefüllt sind und
+	 * der Inhalt auch der regEx entspricht. Wenn Ja, dann hole die Werte aus den
+	 * Felder und erzeuge ein neues Port-Objekt. Initialisiere dent
+	 * ContentFileHandler. Überprüfe, ob es der Status auf Bearbeiten gesetzt wurde.
+	 * Wenn Ja, dann lösche den alten Port und füge den neuen Port in die Datei.
+	 * Wenn Nein, dann füge nur den eingebenen Port hinzu. Gib eine Nachricht aus
+	 * und mache eine Update der PortChoiceBox. Leere alle Felder.
+	 */
 	public void savePortEntry() {
 		if (isPortFieldNotEmptyAndValid()) {
 			pNew = new Port(portNameTField.getText());
 			pNew.createPort(portAddrTField.getText());
 			jfList.init();
-			if (isPortEditActiv() && isPortChoiceBoxActiv()) {
-				editPortEntry();
-				setPortEditActiv(false);
-				setPortChoiceBoxActiv(false);
-			} else {
-				jfList.addPort(pNew);
-				System.out.println(jfList.getPortsArray());
-				messageLabel.setText(jfList.getExcMessage());
+			if (isPortEditActiv) {
+				jfList.deletePort(pOld.getName());
 			}
+			jfList.addPort(pNew);
+			System.out.println(jfList.getPortsArray());
+			messageLabel.setText(jfList.getExcMessage());
 			updatePortList();
 		}
 		clearPortField();
 	}
+	// #########################################################################
+	// ## Server ###############################################################
+	// #########################################################################
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		jfList.init();
-		updatePortList();
+	/**
+	 * Überprüft ob gültige Werte in den Feldern geschrieben wurden.
+	 * 
+	 * @return true wenn Feld nicht leer und gültige IP-Adresse
+	 */
+	private boolean isSAddrFieldValid() {
+		if (!serverNameTField.getText().isEmpty() || !ipAddr1TField.getText().isEmpty()
+				|| !ipAddr2TField.getText().isEmpty() || !ipAddr3TField.getText().isEmpty()
+				|| !ipAddr4TField.getText().isEmpty()) {
+			ipTFieldConcat = ipAddr1TField.getText() + "." + ipAddr2TField.getText() + "." + ipAddr3TField.getText()
+					+ "." + ipAddr4TField.getText();
+			if (rv.validateIP(ipTFieldConcat)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	/**
+	 * Leere alle Server-Eingabefelder.
+	 */
+	public void clearServerField() {
+		serverNameTField.clear();
+		ipAddr1TField.clear();
+		ipAddr2TField.clear();
+		ipAddr3TField.clear();
+		ipAddr4TField.clear();
+	}
+	/**
+	 * Aktualisiere den Inhalt der Server-ChoiceBox.
+	 */
+	public void updateServerList() {
+		jfList.saveServerValuesInAList();
+		serverChoiceBox.setItems(jfList.getServerNameList());
+	}
+
+	/**
+	 * Lösche einen Server aus der JSON-Datei.
+	 */
+	// << Delete >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	public void delServerEntry() {
+		jfList.deleteServer(serverChoiceBox.getValue());
 		updateServerList();
+	}
+
+	// << Edit >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	
+
+	public void editServerEntry() {
+		
+	}
+
+	// << Add >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	public void saveServerEntry() {
+		if (isSAddrFieldValid()) {
+			sNew = new Server(serverNameTField.getText());
+			sNew.setIPcreateHost(ipTFieldConcat);
+			jfList.init();
+			System.out.println(isServerChoiceBoxActiv() && isServerEditActiv());
+			if (isServerChoiceBoxActiv() && isServerEditActiv()) {
+				editServerEntry();
+				setServerChoiceBoxActiv(false);
+				setServerEditActiv(false);
+			}
+			jfList.addServer(sNew);
+			updateServerList();
+			System.out.println(jfList.getServerArray());
+			messageLabel.setText(jfList.getExcMessage());
+		}
+		clearServerField();
 	}
 
 	// #########################################################################
@@ -237,25 +241,18 @@ public class SampleController implements Initializable {
 		this.isPortEditActiv = editButtonWasPressed;
 	}
 
-	public boolean isPortChoiceBoxActiv() {
-		return isPortChoiceBoxActiv;
-	}
-
-	public void setPortChoiceBoxActiv(boolean isChoiceBoxActiv) {
-		this.isPortChoiceBoxActiv = isChoiceBoxActiv;
-	}
-	
 	public boolean isServerEditActiv() {
 		return isServerEditActiv;
 	}
+
 	public boolean isServerChoiceBoxActiv() {
 		return isServerChoiceBoxActiv;
 	}
-	
+
 	public void setServerChoiceBoxActiv(boolean isServerChoiceBoxActiv) {
 		this.isServerChoiceBoxActiv = isServerChoiceBoxActiv;
 	}
-	
+
 	public void setServerEditActiv(boolean isServerEditActiv) {
 		this.isServerEditActiv = isServerEditActiv;
 	}
