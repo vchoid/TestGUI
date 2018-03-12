305378dd7188c3a0917b2d20b6c4ebba1f0f7ae7
Christoph Kiank <christoph.kiank@bitmarck.de>
2018-03-12 15:45:12 +0100

    Port Edit mit Disable Text Fields funktionuiert

--- src/application/ViewController.java
+++ src/application/ViewController.java
@@ -3,6 +3,8 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -34,7 +36,7 @@ public class ViewController implements Initializable {
 	private ComboBox<String> serverComboBox;
 	@FXML
 	private Label serverPH;
-	
+
 	private String ipTFieldConcat;
 	private Server sOld;
 	private Server sNew;
@@ -48,13 +50,13 @@ public class ViewController implements Initializable {
 	private TextField portAddrTField;
 	@FXML
 	private ComboBox<String> portComboBox;
-	@FXML Label portPH;
-	
+	@FXML
+	Label portPH;
+
 	private Port pOld;
 	private Port pNew;
 	private boolean isPortEditActiv;
-	private boolean isPortChoiceMade;
-
+	private boolean isPortChoose;
 
 	// #########################################################################
 	// ## Init #################################################################
@@ -80,14 +82,16 @@ public class ViewController implements Initializable {
 		portPH.setVisible(true);
 	}
 	/**
-	 * Überprüft ob die die Felder nicht leer sind. Wenn ja dann lege einen neuen
-	 * Portnamen an. Lege einen einen neuen Port an, welcher beim erzeugen validiert
-	 * wird und ein true oder false zurück gibt. Bei false leere die Eingabefelder
+	 * Überprüft ob die die Felder nicht leer sind. Wenn ja dann lege einen
+	 * neuen Portnamen an. Lege einen einen neuen Port an, welcher beim erzeugen
+	 * validiert wird und ein true oder false zurück gibt. Bei false leere die
+	 * Eingabefelder
 	 * 
 	 * @return
 	 */
 	private boolean isPortFieldNotEmptyAndValid() {
-		if (!portNameTField.getText().isEmpty() || !portAddrTField.getText().isEmpty()) {
+		if (!portNameTField.getText().isEmpty()
+				|| !portAddrTField.getText().isEmpty()) {
 			pNew = new Port(portNameTField.getText());
 			return pNew.createValidPort(portAddrTField.getText());
 		}
@@ -101,8 +105,9 @@ public class ViewController implements Initializable {
 	public void clearPortField() {
 		portNameTField.clear();
 		portAddrTField.clear();
-		updatePortList();
 		enablePortPH();
+		updatePortList();
+		setPortFieldEditable(true);
 	}
 
 	/**
@@ -113,16 +118,28 @@ public class ViewController implements Initializable {
 		portComboBox.setItems(jfList.getPortNameList());
 		isPortEditActiv = false;
 	}
-	public void setPortChoice() {
-		isPortChoiceMade = true;
-	}
 
+	public void addPortValuesFromComboToTFields() {
+		pOld = new Port(portComboBox.getValue());
+		pOld.createValidPort(jfList.searchValueByName(jfList.getPortsArray(),
+				pOld.getName(), "port"));
+		portNameTField.setText(pOld.getName());
+		portAddrTField.setText(pOld.getPort());
+		isPortChoose = true;
+		setPortFieldEditable(false);
+	}
+	
+	public void setPortFieldEditable(boolean bool) {
+		portNameTField.setDisable(!bool);
+		portAddrTField.setDisable(!bool);
+		
+	}
 	// << Delete >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	/**
 	 * Löscht einen Port aus der JSON-Datei
 	 */
 	public void delportEntry() {
-		if(isPortChoiceMade) {
+		if (isPortChoose) {
 			jfList.deletePort(portComboBox.getValue());
 		}
 		clearPortField();
@@ -134,24 +151,21 @@ public class ViewController implements Initializable {
 	 * Anschließend wird der Status Bearbeiten auf true gesetzt,
 	 */
 	public void editPortEntry() {
-		if(isPortChoiceMade) {
-			pOld = new Port(portComboBox.getValue());
-			pOld.createValidPort(jfList.searchValueByName(jfList.getPortsArray(), pOld.getName(), "port"));
-			portNameTField.setText(pOld.getName());
-			portAddrTField.setText(pOld.getPort());
+		if (isPortChoose) {
 			isPortEditActiv = true;
-		} 
+			setPortFieldEditable(true);
+		}
 	}
 
 	// << Add >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	/**
-	 * Überprüft vor dem Einfügen eines neuen Ports ob die Felder gefüllt sind und
-	 * der Inhalt auch der regEx entspricht. Wenn ja, dann hole die Werte aus den
-	 * Felder und erzeuge ein neues Port-Objekt. Initialisiere dent
-	 * ContentFileHandler. Überprüfe, ob es der Status auf Bearbeiten gesetzt wurde.
-	 * Wenn ja, dann lösche den alten Port und füge den neuen Port in die Datei.
-	 * Wenn nein, dann füge nur den eingebenen Port hinzu. Gib eine Nachricht aus
-	 * und mache eine Update der PortChoiceBox. Leere alle Felder.
+	 * Überprüft vor dem Einfügen eines neuen Ports ob die Felder gefüllt sind
+	 * und der Inhalt auch der regEx entspricht. Wenn ja, dann hole die Werte
+	 * aus den Felder und erzeuge ein neues Port-Objekt. Initialisiere dent
+	 * ContentFileHandler. Überprüfe, ob es der Status auf Bearbeiten gesetzt
+	 * wurde. Wenn ja, dann lösche den alten Port und füge den neuen Port in die
+	 * Datei. Wenn nein, dann füge nur den eingebenen Port hinzu. Gib eine
+	 * Nachricht aus und mache eine Update der PortChoiceBox. Leere alle Felder.
 	 */
 	public void savePortEntry() {
 		if (isPortFieldNotEmptyAndValid()) {
@@ -177,19 +191,22 @@ public class ViewController implements Initializable {
 	}
 	/**
 	 * Überprüft ob in allen Eingabefeldern Werte geschrieben wurden. Wenn ja,
-	 * erzeuge einen neuen Servernamen, hole den Text aus dem Eingabefeld. Füge alle
-	 * Eingabefelder für die IP-Adresse zusammen zu einer IP. Erstelle eine neue IP.
-	 * Wenn es sich um eine valide IP handelt gibt diese zurück und ein true.
-	 * Automatisch wird ein Ip-Array erzeugt. Bei false leere alle Felder.
+	 * erzeuge einen neuen Servernamen, hole den Text aus dem Eingabefeld. Füge
+	 * alle Eingabefelder für die IP-Adresse zusammen zu einer IP. Erstelle eine
+	 * neue IP. Wenn es sich um eine valide IP handelt gibt diese zurück und ein
+	 * true. Automatisch wird ein Ip-Array erzeugt. Bei false leere alle Felder.
 	 * 
 	 * @return true wenn Feld nicht leer und gültige IP-Adresse
 	 */
 	private boolean isSAddrFieldValid() {
-		if (!serverNameTField.getText().isEmpty() || !ipAddr0TField.getText().isEmpty()
-				|| !ipAddr1TField.getText().isEmpty() || !ipAddr2TField.getText().isEmpty()
+		if (!serverNameTField.getText().isEmpty()
+				|| !ipAddr0TField.getText().isEmpty()
+				|| !ipAddr1TField.getText().isEmpty()
+				|| !ipAddr2TField.getText().isEmpty()
 				|| !ipAddr3TField.getText().isEmpty()) {
 			sNew = new Server(serverNameTField.getText());
-			ipTFieldConcat = ipAddr0TField.getText() + "." + ipAddr1TField.getText() + "." + ipAddr2TField.getText()
+			ipTFieldConcat = ipAddr0TField.getText() + "."
+					+ ipAddr1TField.getText() + "." + ipAddr2TField.getText()
 					+ "." + ipAddr3TField.getText();
 			return sNew.createValidIpWithArrayGetHost(ipTFieldConcat);
 		}
@@ -227,7 +244,7 @@ public class ViewController implements Initializable {
 	 * Lösche einen Server aus der JSON-Datei.
 	 */
 	public void delServerEntry() {
-		if(isServerChoiceMade) {
+		if (isServerChoiceMade) {
 			jfList.deleteServer(serverComboBox.getValue());
 		}
 		clearServerField();
@@ -235,16 +252,17 @@ public class ViewController implements Initializable {
 
 	// << Edit >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	public void editServerEntry() {
-		if(isServerChoiceMade) {
+		if (isServerChoiceMade) {
 			sOld = new Server(serverComboBox.getValue());
-			sOld.createValidIpWithArrayGetHost(jfList.searchValueByName(jfList.getServerArray(), sOld.getName(), "ip"));
+			sOld.createValidIpWithArrayGetHost(jfList.searchValueByName(
+					jfList.getServerArray(), sOld.getName(), "ip"));
 			serverNameTField.setText(sOld.getName());
 			ipAddr0TField.setText(sOld.getIpArr()[0]);
 			ipAddr1TField.setText(sOld.getIpArr()[1]);
 			ipAddr2TField.setText(sOld.getIpArr()[2]);
 			ipAddr3TField.setText(sOld.getIpArr()[3]);
 			isServerEditActiv = true;
-		} 
+		}
 	}
 
 	// << Add >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -261,6 +279,5 @@ public class ViewController implements Initializable {
 		}
 		clearServerField();
 	}
-	
-	
+
 }
